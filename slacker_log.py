5f2329c9afd46eda2879885ff993ceb2b3e8d7d8
hayato <hayatotsukiji@gmail.com>
2017-04-26 17:20:14 +0900

    ログをJSON形式にして出力するように更新(未検証)

--- slacker_log.py
+++ slacker_log.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import sys,codecs
+import sys,codecs,json
 from slacker import Slacker
 
 # API token tokenが入ったファイルの読み込みをして取得(セキュリティ回避)
@@ -12,10 +12,21 @@ c_name = 'C0J8KM6KF'
 # 投稿する画像ファイルへのパス(パラメタから取得) 相対パス
 #f_path = sys.argv[1]
  
-# 投稿
+# logの取得を送る　token,指定チャンネルid,タイムスタンプの有無などを条件付けする
+# 返しはインスタンス変数になってる(？)
 slacker = Slacker(token)
-result = slacker.channels.history(c_name,inclusive=True,unreads=True)
-#slacker.files.upload(f_path, channels=[c_name], title='タイトル')
+result = slacker.channels.history(c_name,count=5,inclusive=True,unreads=True)
+
+#listに突っ込む
+log = []
+log.append(result)
 
 
-print(vars(result))
+f = codecs.open("slack.json","w","utf-8")
+
+f.write(json.dumps(log));
+
+
+
+#とりあえず表示させる
+#print(vars(result))
