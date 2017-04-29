1b9203390fbebc968381c0665f201be1b3c71618
HayatoTsukiji <hayatotsukiji@gmail.com>
2017-04-29 17:16:24 +0900

    test(lenovoかつpychameでやれているか確認)

--- log_analysis.py
+++ log_analysis.py
@@ -5,3 +5,19 @@ import codecs,json
 #ログのJSONファイルの読み込み
 f = codecs.open("slack.json","r","utf-8")
 f_json = json.load(f)
+
+index_username = []
+index = []
+count = 0
+
+#隔離
+#まずはf_json["messages"]["user"]の中身が取れているのか確認
+"""
+for i in f_json["messages"]:
+    if f_json["messages"]["user"] in index_username:
+        index_username.append(f_json["messages"]["user"])
+    else:
+        count += 1
+
+print(count)
+"""
