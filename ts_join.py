b0ead192026332393248ec2419080bcc931f819c
HayatoTsukiji <hayatotsukiji@gmail.com>
2017-05-29 16:11:10 +0900

    ts_joinの作成

--- ts_join.py
+++ ts_join.py
@@ -1,20 +1,51 @@
 # -*- coding: utf-8 -*-
 
-import codecs, json, csv
-from datetime import datetime
+import codecs, json, csv, datetime
 #ログのJSONファイルの読み込み
-f = open("log_general_channel.csv","r")
-fgeneral = csv.reader(f)
-f = open("log_report_channel.csv","r")
-freport = csv.reader(f)
-f = open("log_seminar1421_channel.csv","r")
-fseminar = csv.reader(f)
-f4 = open("log_grad2017_channel.csv","rb")
-fgrad = csv.reader(f4)
+f = codecs.open("slack_C0J8KM6KF.json","r","utf-8")
+f1 = json.load(f)
+f = codecs.open("slack_C0XMH9F0Q.json","r","utf-8")
+f2 = json.load(f)
+f = codecs.open("slack_C4W2RL2BA.json","r","utf-8")
+f3 = json.load(f)
+f = codecs.open("slack_C18JT8ZNY.json","r","utf-8")
+f4 = json.load(f)
 fm = codecs.open("memberlist.json","r","utf-8")
 memberlist = json.load(fm)
 
-print(fgrad)
+month = []
 
+for i in f1["messages"]:
+    d = datetime.datetime.fromtimestamp(float(i["ts"]))
+    for j in range(12):
+        if d.month == j+1:
+            month.append({j+1:{"user": i["user"], "text": i["text"], "date": d, "ts": i["ts"], "channel": "f1"}})
 
+for i in f2["messages"]:
+    d = datetime.datetime.fromtimestamp(float(i["ts"]))
+    for j in range(12):
+        if d.month == j+1:
+            month.append({j+1:{"user": i["user"], "text": i["text"], "date": d, "ts": i["ts"], "channel": "f2"}})
 
+for i in f3["messages"]:
+    d = datetime.datetime.fromtimestamp(float(i["ts"]))
+    for j in range(12):
+        if d.month == j+1:
+            month.append({j+1:{"user": i["user"], "text": i["text"], "date": d, "ts": i["ts"], "channel": "f3"}})
+
+for i in f4["messages"]:
+    if (i.get("subtype") == "file_comment"):
+        continue
+    d = datetime.datetime.fromtimestamp(float(i["ts"]))
+    for j in range(12):
+        if d.month == j+1:
+            month.append({j+1:{"user": i["user"], "text": i["text"], "date": d, "ts": i["ts"], "channel": "f4"}})
+
+print(month)
+
+"""
+#JSON形式に変換しファイルに出力
+f = codecs.open("test.json","w","utf-8")
+f.write(json.dumps(month));
+f.close()
+"""
