4b4a1f45971af51bfd7f2ad33af5cdadccbd14e7
HayatoTsukiji <hayatotsukiji@gmail.com>
2017-09-06 20:30:29 +0900

    OCno

--- oc2017/word2vec.php
+++ oc2017/word2vec.php
@@ -1,9 +1,15 @@
 <?php
 
 #ajax動作テストhelloを返す
-$word = $_POST["word"];
 
-$data = "hello";
+$word = $_POST["word"];
+$word = mb_convert_encoding($word, "SJIS");
+
+exec("python word2vec_similarity.py ".$word);
+
+#これはあとでやるたぶんコロンが悪い
+#$data = @file("word2vec_result.csv");
+$data = file_get_contents("word2vec_result.csv");
 
 print json_encode("$data");
 
--- oc2017/word2vec_similarity.py
+++ oc2017/word2vec_similarity.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from gensim.models import word2vec
-import sys
+import sys,csv
 
 model   = word2vec.Word2Vec.load('word2vec-gensim-model/word2vec.gensim.model')
 results = model.most_similar(positive=sys.argv[1],negative="", topn=10)
 
-for result in results:
-    print(result[0], '\t', result[1])
+try:
+    # 書き込み UTF-8
+    with open('word2vec_result.csv', 'w', encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile, lineterminator='\n')
+        writer.writerow(['word', 'score'])
+        for i in results:
+            writer.writerow([i[0],i[1]])
 
-#print(model[sys.argv[1]])
+# 起こりそうな例外をキャッチ
+except FileNotFoundError as e:
+    print(e)
+except csv.Error as e:
+    print(e)
