4714ca74fc406fb068d00e61aa9224276d14758f
HayatoTsukiji <hayatotsukiji@gmail.com>
2017-09-14 15:18:30 +0900

    ユーザ毎に履歴を分類するプログラムと各ユーザがあるユーザを指定した回数と相手の一覧を抽出するプログラムの作成開始(バックアップ)

--- userlog.py
+++ userlog.py
@@ -8,6 +8,10 @@ file = open(inputfilename, 'r', encoding="utf-8")
 inputfilename = re.sub("\.[a-z]+","",inputfilename)
 csvdata = csv.reader(file)
 
+logindex = []
 
 for row in csvdata:
-    print(row[0])
+    userfile = open("txt/"+row[0]+".txt","a",encoding="utf-8")
+    userfile.write(row[1]+","+row[2]+","+row[3]+"\n")
+
+userfile.close()
