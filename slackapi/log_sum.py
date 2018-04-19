3b2f63754fc8a146092b0e5ee022f65a25fa250c
HayatoTsukiji <hayatotsukiji@gmail.com>
2018-04-19 22:08:50 +0900

    機能拡張(途中)

--- slackapi/log_sum.py
+++ slackapi/log_sum.py
@@ -10,16 +10,37 @@ files = glob.glob('../json/20180419/*.json')
 fm = codecs.open("memberlist.json","r","utf-8")
 memberlist = json.load(fm)
 
-sumlog = []
+
+def append_index(index, username, text, ts, date_ts):
+    index.append([username, text, ts, date_ts])
+
+
 
 #csvファイルに発言、時間、人のデータを書き込む
 #member.jsonでユーザ名に変換
+logs = []
 for f in files:
     # ログのJSONファイルの読み込み
     fl = codecs.open(f, "r", "utf-8")
     f_json = json.load(fl)
+
     try:
         for i in f_json["messages"]:
+            if ('user' in i ) and ( 'text' in i ):
+                append_index(logs, i['user'], i['text'], float(i['ts']), datetime.fromtimestamp(float(i["ts"])))
+                continue
+
+            elif ('user' not in i ) or ('text' not in i):
+                if 'bot_id' in i: 
+                    append_index(logs, i['bot_id'], i['text'], float(i['ts']), datetime.fromtimestamp(float(i["ts"])))         
+                    continue
+
+                if 'comment' in i:
+                    #print(i['comment']['user'], i['comment']['comment'], i['comment']['timestamp'])
+                    append_index(logs, i['comment']['user'], i['comment']['comment'], float(i['comment']['timestamp']), datetime.fromtimestamp(float(i['comment']['timestamp'])))
+                    continue
+
+            '''
             if(i.get("subtype") == "file_share"):
                 continue
             elif(i.get("subtype") == "bot_add"):
@@ -39,10 +60,13 @@ for f in files:
                         username = j["name"]
                         s_text = i["text"]
             sumlog.append([username, s_text, float(i["ts"]), datetime.fromtimestamp(float(i["ts"]))])
+            '''
     # 起こりそうな例外をキャッチ
     except FileNotFoundError as e:
         print(e)
 
+
+'''
 sumlog = sorted(sumlog, key=lambda x:(x[0],x[2]))
 
 with open('slacklog.csv', 'w', encoding="utf-8") as csvfile:
@@ -50,3 +74,4 @@ with open('slacklog.csv', 'w', encoding="utf-8") as csvfile:
     writer.writerow(['User', 'text', 'timestamp', 'time'])
     for i in sumlog:
         writer.writerow([i[0], i[1], i[2], i[3]])
+'''
