592e0a3b2007d4165504c652b704fb045aa37846
HayatoTsukiji <hayatotsukiji@gmail.com>
2018-04-20 11:20:09 +0900

    ログを辞書に突っ込む部分を関数化

--- slackapi/log_sum.py
+++ slackapi/log_sum.py
@@ -15,15 +15,7 @@ def append_index(index, username, text, ts, date_ts):
     index.append([username, text, ts, date_ts])
 
 
-
-#csvファイルに発言、時間、人のデータを書き込む
-#member.jsonでユーザ名に変換
-logs = []
-for f in files:
-    # ログのJSONファイルの読み込み
-    fl = codecs.open(f, "r", "utf-8")
-    f_json = json.load(fl)
-
+def loggets():
     try:
         for i in f_json["messages"]:
             if ('user' in i ) and ( 'text' in i ):
@@ -40,38 +32,50 @@ for f in files:
                     append_index(logs, i['comment']['user'], i['comment']['comment'], float(i['comment']['timestamp']), datetime.fromtimestamp(float(i['comment']['timestamp'])))
                     continue
 
-            '''
-            if(i.get("subtype") == "file_share"):
-                continue
-            elif(i.get("subtype") == "bot_add"):
-                continue
-            elif(i.get("subtype") == "bot_message"):
-                username = i["bot_id"]
-                for at in i["attachments"]:
-                    s_text = at["text"]
-            elif(i.get("subtype") == "file_comment"):
-                for j in memberlist:
-                    if (i['comment']['user'] in j["id"]) == True:
-                        username = j["name"]
-                        s_text = i["comment"]["comment"]
-            else:           
-                for j in memberlist:
-                    if (i['user'] in j["id"]) == True:
-                        username = j["name"]
-                        s_text = i["text"]
-            sumlog.append([username, s_text, float(i["ts"]), datetime.fromtimestamp(float(i["ts"]))])
-            '''
     # 起こりそうな例外をキャッチ
     except FileNotFoundError as e:
         print(e)
 
+#まだ一応残しておく
+    '''
+    if(i.get("subtype") == "file_share"):
+        continue
+    elif(i.get("subtype") == "bot_add"):
+        continue
+    elif(i.get("subtype") == "bot_message"):
+        username = i["bot_id"]
+        for at in i["attachments"]:
+            s_text = at["text"]
+    elif(i.get("subtype") == "file_comment"):
+        for j in memberlist:
+            if (i['comment']['user'] in j["id"]) == True:
+                username = j["name"]
+                s_text = i["comment"]["comment"]
+    else:           
+        for j in memberlist:
+            if (i['user'] in j["id"]) == True:
+                username = j["name"]
+                s_text = i["text"]
+    sumlog.append([username, s_text, float(i["ts"]), datetime.fromtimestamp(float(i["ts"]))])
+    '''
+    
 
-'''
-sumlog = sorted(sumlog, key=lambda x:(x[0],x[2]))
+if __name__ == '__main__':
+    #csvファイルに発言、時間、人のデータを書き込む
+    #member.jsonでユーザ名に変換
+    logs = []
+    for f in files:
+        # ログのJSONファイルの読み込み
+        fl = codecs.open(f, "r", "utf-8")
+        f_json = json.load(fl)
 
-with open('slacklog.csv', 'w', encoding="utf-8") as csvfile:
-    writer = csv.writer(csvfile, lineterminator='\n')
-    writer.writerow(['User', 'text', 'timestamp', 'time'])
-    for i in sumlog:
-        writer.writerow([i[0], i[1], i[2], i[3]])
-'''
+
+    '''
+    sumlog = sorted(sumlog, key=lambda x:(x[0],x[2]))
+
+    with open('slacklog.csv', 'w', encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile, lineterminator='\n')
+        writer.writerow(['User', 'text', 'timestamp', 'time'])
+        for i in sumlog:
+            writer.writerow([i[0], i[1], i[2], i[3]])
+    '''
