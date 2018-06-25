db772a5046dcf2ec5590fbd3429792229b36fea2
HayatoTsukiji <hayatotsukiji@gmail.com>
2018-06-25 15:19:35 +0900

    データ追加

--- comment evaluation/change_mojicode.py
+++ comment evaluation/change_mojicode.py
@@ -19,15 +19,17 @@ for file in txt_filelist:
     shiftjis_csv_path = file
 
     # UTF-8 ファイルのパス
-    utf8_csv_path = filepath+"test.txt"
-
-    print(file)
+    utf8_csv_path = file
 
     # 文字コードを utf-8 に変換して保存
     fin = codecs.open(shiftjis_csv_path, "r", "shift_jis")
-    fout_utf = codecs.open(utf8_csv_path, "w", "utf-8")
+    tmp = []
     for row in fin:
-        fout_utf.write(row)
+        tmp.append(row)
     fin.close()
+
+    fout_utf = codecs.open(utf8_csv_path, "w", "utf-8")
+    for row in tmp:
+        fout_utf.write(row)
     fout_utf.close()
     
